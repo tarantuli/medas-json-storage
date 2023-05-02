8a195c50053f2f1f61894606fdfe534bb19a9445
Patrick Morpey <patrick@morphp.nl>
2023-05-02 12:49:52 +0200

    Replaced placeholders

--- src/JsonStoragePackage.php
+++ src/JsonStoragePackage.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Medas\Placeholder;
+namespace Medas\JsonStorage;
 
 use Medas\Core\AsSingleton;
 use Medas\ServiceManager\BasePackage;
 
-class PlaceholderPackage extends BasePackage
+class JsonStoragePackage extends BasePackage
 {
     use AsSingleton;
 
