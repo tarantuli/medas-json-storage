7a48f8212b45f5ebaf3c3f426149e06abaf1a8ff
Patrick Morpey <patrick@morphp.nl>
2026-07-20 10:39:36 +0200

    Added `DirectoryCreator` dependency to `CreateStoreExecutor` and ensured directory creation before writing content.

--- src/Actions/CreateStore/CreateStoreExecutor.php
+++ src/Actions/CreateStore/CreateStoreExecutor.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace Medas\JsonStorage\Actions\CreateStore;
 
-use Medas\Core\Attributes\Service;
+use Medas\Core\{Attributes\Service, Interfaces\DirectoryCreator};
 
 #[Service]
 readonly class CreateStoreExecutor
 {
+    public function __construct(
+        private DirectoryCreator $directoryCreator,
+    )
+    {
+    }
+
     public function execute(CreateStore $action): void
     {
+        $this->directoryCreator->create(dirname($action->path));
+
         file_put_contents($action->path, $action->content);
     }
 }
